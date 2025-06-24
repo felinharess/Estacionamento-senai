f857aa4a49b41c6a40ff2659af4fae7307f0f6ae
felinharess <felinharesdomingues@gmail.com>
2025-06-24 19:51:25 -0300

    Estacionamento-Senai

--- back-end/routes/acessoRoutes.js
+++ back-end/routes/acessoRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/entrada', registrarEntrada); 
 router.put('/saida/:id_acesso', registrarSaida); 
-router.get('/acessos/ativos/:id_veiculo', buscarAcessoAtivo);
+router.get('/ativos/:id_veiculo', buscarAcessoAtivo);
 
 export default router;
